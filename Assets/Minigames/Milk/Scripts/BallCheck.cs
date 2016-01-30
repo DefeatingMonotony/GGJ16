64edd330e010d7ad623318fa8fef6cf7661743b6
codercurtis <codercurtis@gmail.com>
2016-01-30 09:00:01 -0700

    Milk minigame

---
+++ Assets/Minigames/Milk/Scripts/BallCheck.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallCheck : MonoBehaviour {
+	
+	public int balls = 5;
+	
+	void Update(){
+		if(balls<=0){
+			Debug.Log("winnar");
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col){
+		balls--;
+	}
+}
---
+++ Assets/Minigames/Milk/Scripts/MouseRotate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseRotate : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnMouseDrag(){		
+		//http://answers.unity3d.com/questions/585035/lookat-2d-equivalent-.html
+		Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.parent.position;
+		diff.Normalize();
+		
+		float rot_z = Mathf.Atan2(diff.y,diff.x) * Mathf.Rad2Deg;
+		transform.parent.rotation = Quaternion.Euler(0f,0f,rot_z);
+	}
+}
