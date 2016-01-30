29344c3e9af1a0da04d6f7a2f33896d0e8e61f80
codercurtis <codercurtis@gmail.com>
2016-01-30 15:18:08 -0700

    Saving this rope
    This rope is important to me.

---
+++ Assets/Minigames/Die/Scripts/MouseDragParent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseDragParent : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnMouseDrag(){
+		Vector3 newPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,0));
+		newPos.z = 0;
+		transform.parent.position = newPos;
+	}
+}
