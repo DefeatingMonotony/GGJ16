bf927c89fac9a17fc469b0a98a636c254fbd320e
codercurtis <codercurtis@gmail.com>
2016-01-29 23:14:03 -0700

    Handshake

---
+++ Assets/Minigames/Handshake/Scripts/CollideSnap.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollideSnap : MonoBehaviour {
+
+	public bool snapped = false;
+	public float score = 100;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(snapped){
+			score -= (Mathf.Abs(Input.GetAxis("Mouse X")) + Mathf.Abs(Input.GetAxis("Mouse Y")));
+			if(score<=0){
+				Debug.Log("You win");
+			}
+		}
+	}
+	
+	void OnTriggerEnter2D(Collider2D col){
+		if(col.gameObject.name == "TargetHand"){
+			col.gameObject.transform.parent.transform.parent = transform.parent;
+			snapped = true;
+		}
+	}
+}
