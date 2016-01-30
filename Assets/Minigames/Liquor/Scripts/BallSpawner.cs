aa2a9e884d30d83839a79e87be37567189cb7b06
codercurtis <codercurtis@gmail.com>
2016-01-30 10:27:51 -0700

    Liquor

---
+++ Assets/Minigames/Liquor/Scripts/BallSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallSpawner : MonoBehaviour {
+	public GameObject ball;
+	public float interval;
+	bool runOnce;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if((int)(Time.timeSinceLevelLoad) % interval == 0){
+			if(!runOnce){
+				runOnce = true;
+				spawnBalls(10);
+			}
+		}else{
+			runOnce = false;
+		}
+	}
+	
+	void spawnBalls(int num){
+			for (int i=0;i<num;i++){
+				GameObject.Instantiate(ball,transform.position,Quaternion.identity);
+			}
+		}
+}
---
+++ Assets/Minigames/Liquor/Scripts/CountTriggerEnter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CountTriggerEnter : MonoBehaviour {
+
+	public int goalNum;
+	public int triggerCount;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(triggerCount >= goalNum){
+			Debug.Log("win");
+		}
+	}
+	
+	void OnTriggerEnter2D(Collider2D col){
+		triggerCount++;
+	}
+}
