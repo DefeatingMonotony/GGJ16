0e9e435ea6b1013c716c713a409283e290b1187c
LRFLEW <lrflew@aol.com>
2016-01-30 16:41:14 -0700

    BlockOut

---
+++ Assets/Minigames/BlockOut/BlockOut.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockOut : MonoBehaviour {
+	public Transform circle;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseDown () {
+		Vector3 mouse = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		Transform blob = Instantiate(circle, new Vector3(mouse.x, mouse.y, 1.0f), Quaternion.identity) as Transform;
+		GameObject cover = GameObject.Find("CoverTrigger");
+		if (blob.GetComponent<BoxCollider2D>().bounds.Intersects(cover.GetComponent<BoxCollider2D>().bounds)) {
+			cover.GetComponent<Cover>().HandleDraw(blob);
+		}
+	}
+
+	void OnMouseDrag () {
+		Vector3 mouse = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		Transform blob = Instantiate(circle, new Vector3(mouse.x, mouse.y, 1.0f), Quaternion.identity) as Transform;
+		GameObject cover = GameObject.Find("CoverTrigger");
+		if (blob.GetComponent<BoxCollider2D>().bounds.Intersects(cover.GetComponent<BoxCollider2D>().bounds)) {
+			cover.GetComponent<Cover>().HandleDraw(blob);
+		}
+	}
+}
---
+++ Assets/Minigames/BlockOut/Cover.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cover : MonoBehaviour {
+	private float xmin, xmax, ymin, ymax;
+
+	// Use this for initialization
+	void Start () {
+		xmin = xmax = ymin = ymax = float.NaN;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Bounds b = GetComponent<BoxCollider2D>().bounds;
+		if (xmin <= b.min.x && xmax >= b.max.x && ymin <= b.min.y && ymax >= b.max.y) {
+			Debug.Log("Winner");
+		}
+	}
+
+	public void HandleDraw(Transform col){
+		Vector3 pos = col.position;
+		Vector2 size = col.GetComponent<BoxCollider2D>().bounds.size;
+
+		if (float.IsNaN(xmin)) xmin = pos.x - (size.x / 2.0f);
+		else xmin = Mathf.Min(xmin, pos.x - (size.x / 2.0f));
+		if (float.IsNaN(xmax)) xmax = pos.x + (size.x / 2.0f);
+		else xmax = Mathf.Max(xmax, pos.x + (size.x / 2.0f));
+		if (float.IsNaN(ymin)) ymin = pos.y - (size.y / 2.0f);
+		else ymin = Mathf.Min(ymin, pos.y - (size.y / 2.0f));
+		if (float.IsNaN(ymax)) ymax = pos.y + (size.y / 2.0f);
+		else ymax = Mathf.Max(ymax, pos.y + (size.y / 2.0f));
+	}
+}
