edb4ec29638a0fc4ebc4e45165fe4afddbe458dd
codercurtis <codercurtis@gmail.com>
2016-01-30 09:42:45 -0700

    Dread minigame

---
+++ Assets/Minigames/Dread/Scripts/Follow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+	
+	public GameObject target;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp(transform.position, target.transform.position,speed);
+		transform.position = new Vector3(transform.position.x,transform.position.y,-0.03f);
+	}
+}
---
+++ Assets/Minigames/Dread/Scripts/Scoring.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scoring : MonoBehaviour {
+
+	public int score = 200;
+	public bool isTriggered = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!isTriggered){
+			score--;
+		}
+		if(score<=0){
+			Debug.Log("win");
+		}
+	}
+	
+	void OnTriggerEnter2D(Collider2D col){
+		isTriggered = true;
+	}
+	
+	void OnTriggerExit2D(Collider2D col){
+		isTriggered = false;
+	}
+}
