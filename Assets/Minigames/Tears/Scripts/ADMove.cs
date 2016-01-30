180e9fa964cb28c3a095964954fc3e681237caee
codercurtis <codercurtis@gmail.com>
2016-01-30 16:27:42 -0700

    Tears

---
+++ Assets/Minigames/Tears/Scripts/ADMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ADMove : MonoBehaviour {
+
+	public float movSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.right * Input.GetAxis("Horizontal") * Time.deltaTime * movSpeed;
+	}
+}
