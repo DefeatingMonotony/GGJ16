395d944f0896570b64432e302e08f9b362bd5a51
codercurtis <codercurtis@gmail.com>
2016-01-31 02:21:42 -0700

    Egg minigame

---
+++ Assets/Minigames/Egg/EggBreak.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EggBreak : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnCollisionEnter2D(){
+		GameObject bot = GameObject.Find("Bottom");
+		GameObject top = GameObject.Find("Top");
+		GameObject yolk = GameObject.Find("Yolk");
+		
+		bot.layer = 0;
+		top.layer = 0;
+		
+		bot.AddComponent<Rigidbody2D>();
+		bot.GetComponent<Rigidbody2D>().interpolation = RigidbodyInterpolation2D.Interpolate;
+		top.AddComponent<Rigidbody2D>();
+		top.GetComponent<Rigidbody2D>().interpolation = RigidbodyInterpolation2D.Interpolate;
+		yolk.AddComponent<Rigidbody2D>();
+		
+		Timer.Win();
+	}
+}
