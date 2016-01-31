3455811f250cb5b9bec58cbfc6a0185f33fe1b04
RollingWithNat20 <RollingWithNat20@Gmail.com>
2016-01-30 17:13:13 -0700

    Dress up game

---
+++ Assets/Minigames/Dress Up/Code/ClickableObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickableObject : MonoBehaviour {
+
+	public bool Clicked;
+	// Use this for initialization
+	void Start () {
+		Clicked = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Clicked) {
+			Clicked = false;
+		}
+	}
+
+	void OnMouseDown()
+	{
+		Clicked = true;
+	}
+}
