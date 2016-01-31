0ff98ac3ed1434c9aa1ca1d6592ffdae2210e99b
LRFLEW <lrflew@aol.com>
2016-01-31 16:42:42 -0700

    Fixed X w/ Mouse

--- Assets/Minigames/XXXXXX/XBehavior.cs
+++ Assets/Minigames/XXXXXX/XBehavior.cs
@@ -20,7 +20,7 @@ public class XBehavior : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update() {
-		if (active && Input.anyKeyDown) {
+		if (active && Input.inputString.Length > 0) {
 			destroyed = true;
 		}
 	}
