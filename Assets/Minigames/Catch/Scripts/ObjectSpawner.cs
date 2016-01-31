2e2900b5470f0185f76dc8cd564411bbf4a65062
codercurtis <codercurtis@gmail.com>
2016-01-30 18:56:26 -0700

    Scrub
    Scrub all you want, Justin
    You'll never be clean

---
+++ Assets/Minigames/Catch/Scripts/ObjectSpawner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectSpawner : MonoBehaviour {
+	public float delay;
+	public float interval;
+	public GameObject obj;
+	
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("SpawnObject",delay,interval);
+	}
+	
+	// Update is called once per frame
+	void SpawnObject(){
+		GameObject.Instantiate(obj,transform.position,transform.rotation);
+	}
+}
---
+++ Assets/Minigames/Scrub/Scripts/TriggerDestroyOverTime.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerDestroyOverTime : MonoBehaviour {
+	
+	public int destroyed;
+	public int destroyGoal;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter2D(Collider2D col){
+		Destroy(col.gameObject);
+		destroyed++;
+		if(destroyed >= destroyGoal){
+			Debug.Log("win");
+			Timer.Win();
+		}
+	}
+}
