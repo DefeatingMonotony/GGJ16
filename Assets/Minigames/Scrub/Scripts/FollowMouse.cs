ec1cd91d73f1c939ae3f5be60f52f7b4fe4f482d
codercurtis <codercurtis@gmail.com>
2016-01-31 04:23:42 -0700

    Scrubfix

--- Assets/Minigames/Scrub/Scripts/FollowMouse.cs
+++ Assets/Minigames/Scrub/Scripts/FollowMouse.cs
@@ -6,14 +6,10 @@ public class FollowMouse : MonoBehaviour {
 	public SpringJoint2D spring;
 	public float dampening = 5.0f;
 
-	void Awake()
-	{
+	void Start(){
 		transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,10));
 		spring = this.gameObject.GetComponent<SpringJoint2D>();
 		spring.enabled = false;
-	}
-	
-	void Start(){
 		GetComponent<Rigidbody2D>().drag = dampening;
 		spring.enabled = true;
 		spring.connectedAnchor = Camera.main.ScreenToWorldPoint (Input.mousePosition);
