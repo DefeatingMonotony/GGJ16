0af24ef06e0d02c3887de36873d02e62b6e036f2
LRFLEW <lrflew@aol.com>
2016-01-31 15:25:09 -0700

    Added Quit Button

---
+++ Assets/QuitButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		#if UNITY_EDITOR || UNITY_WEBPLAYER || UNITY_WEBGL || UNITY_IOS
+		Destroy(gameObject);
+		#endif
+	}
+	
+	// Update is called once per frame
+	public void PressQuit () {
+		Application.Quit();
+	}
+}
