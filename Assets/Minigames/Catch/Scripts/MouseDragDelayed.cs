6e8aa5bc140069725750f436f3ba17b15b763d94
LRFLEW <lrflew@aol.com>
2016-01-30 17:33:56 -0700

    Improved collision detection for Catch

--- Assets/Minigames/Catch/Scripts/MouseDragDelayed.cs
+++ Assets/Minigames/Catch/Scripts/MouseDragDelayed.cs
@@ -3,10 +3,21 @@ using System.Collections;
 
 public class MouseDragDelayed : MonoBehaviour {
 	public float speed;
+	private Vector2 newPos;
+
+	void Start() {
+		newPos = transform.position;
+	}
+
+	void Update() {
+		GetComponent<Rigidbody2D>().MovePosition(Vector2.Lerp(transform.position,newPos,speed * Time.deltaTime));
+	}
 	
 	void OnMouseDrag(){
-		Vector3 newPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,0));
-		newPos.z = 0;
-		transform.position = Vector3.Lerp(transform.position,newPos,speed * Time.deltaTime);
+		newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+
+	void OnMouseUp() {
+		newPos = transform.position;
 	}
 }
