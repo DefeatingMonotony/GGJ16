219f2c55b6a482d15658d581bfd90e05aa4e0360
LRFLEW <lrflew@aol.com>
2016-01-31 01:46:00 -0700

    Fixed scoring for Handshake

--- Assets/Minigames/Handshake/Scripts/MouseDrag.cs
+++ Assets/Minigames/Handshake/Scripts/MouseDrag.cs
@@ -2,19 +2,40 @@
 using System.Collections;
 
 public class MouseDrag : MonoBehaviour {
+	public float score = 100.0f;
+	public bool scoring, held;
 	// Use this for initialization
 	void Start () {
-	
+		scoring = false;
+		held = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(scoring && held){
+			Debug.Log("Testing");
+			score -= (Mathf.Abs(Input.GetAxis("Mouse X")) + Mathf.Abs(Input.GetAxis("Mouse Y")));
+			if(score<=0){
+				Timer.Win();
+			}
+		}
+	}
+
+	void OnMouseDown() {
+		held = true;
+	}
+
+	void OnMouseUp() {
+		held = false;
 	}
 	
 	void OnMouseDrag(){
 		Vector3 newPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,0));
 		newPos.z = 0;
 		transform.position = newPos;
+	}
+
+	public void StartScoring() {
+		scoring = true;
 	}
 }
